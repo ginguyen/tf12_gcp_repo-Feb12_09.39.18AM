18b295aa71b19debdd9bd2d87e1b428bcf5c56ee
Giang Nguyen <ginguyen@paloaltonetworks.com>
2021-02-12 09:39:40 -0800

    Modified files:[main.tf]

--- gcp_compute_firewall_allow_ssh/main.tf
+++ gcp_compute_firewall_allow_ssh/main.tf
@@ -53,3 +53,5 @@ resource "google_compute_firewall" "default_firewall" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2021-02-12 09:39:40.482818]
